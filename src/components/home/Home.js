56e2af0b0845fd4693501e21a9d1d1ef2213f416
dorian <dorianlegan@protonmail.com>
2022-04-27 04:16:49 +0100

    comments section with pagination implemented

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,16 @@
-import useFetch from "../hooks/useFetch";
+import useFetch from "../../hooks/useFetch";
 import PostLink from "../post_link/PostLink";
 import "./home.css";
 
 const Home = () => {
   const [loading, data, refresh] = useFetch("/blog");
-  const posts = data.map((dataItem) => {
-    return <PostLink dataItem={dataItem} />;
-  });
-  return <div className="homeWrap">{posts}</div>;
+  let posts;
+  if (!loading) {
+    posts = data.map((dataItem) => {
+      return <PostLink dataItem={dataItem} />;
+    });
+  }
+  return !loading && <div className="homeWrap">{posts}</div>;
 };
 
 export default Home;
