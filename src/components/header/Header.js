f8872ce3fd35875fe83399f13f278fbab79c253d
dorian <dorianlegan@protonmail.com>
2022-04-28 02:42:31 +0100

    removed show header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,8 @@
 import { Link } from "react-router-dom";
-import useShowHeader from "../../hooks/useShowHeader";
+
 import "./header.css";
 
 const Header = () => {
-  const { showHeader, isTop } = useShowHeader();
-
   return (
     <div className={"header"}>
       <Link to={"/"}>BLOGGER</Link>
